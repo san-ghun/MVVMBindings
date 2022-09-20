03aee989d0802b55f7f63ca288cde07c2ca1925f
Sanghun <po4865@gmail.com>
2022-09-20 21:07:57 +0200

    feat: Update controller to have a table view with fetch data method

--- MVVMBindings/ViewController.swift
+++ MVVMBindings/ViewController.swift
@@ -46,15 +46,64 @@ struct UserListViewModel {
 
 // MARK: - Controller
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    private var viewModel = UserListViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        
+        viewModel.users.bind { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        fetchData()
+    }
+    
+    // MARK: Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.users.value?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = viewModel.users.value?[indexPath.row].name
+        return cell
     }
 
-    func fetch() {
+    func fetchData() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            do {
+                let userModels = try JSONDecoder().decode([User].self, from: data)
+                
+                self.viewModel.users.value = userModels.compactMap({
+                    UserTableViewCellViewModel(name: $0.name)
+                })
+            }
+            catch {
+                
+            }
+        }
+        
+        task.resume()
     }
 
 }
