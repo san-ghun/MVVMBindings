5f20d7e64c4075ada24166414e6cc436bd446731
Sanghun <po4865@gmail.com>
2022-09-20 14:02:27 +0200

    feat: Add a class called Observable

--- MVVMBindings/ViewController.swift
+++ MVVMBindings/ViewController.swift
@@ -7,7 +7,26 @@
 
 import UIKit
 
-// Obserable
+// Observable
+
+class Observable<T> {
+    var value: T? {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T?) {
+        self.value = value
+    }
+    
+    private var listener: ((T?) -> Void)?
+    
+    func bind(_ listener: @escaping (T?) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
 
 // Model
 
